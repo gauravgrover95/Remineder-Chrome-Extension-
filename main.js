5a6fbaf689218e1433105abb612d8dae9026c820
Gaurav Grover <Gaurav Grover>
2016-01-09 12:15:01 +0530

    User extension created & connection to php-server made successfull

---
+++ main.js
@@ -0,0 +1,31 @@
+
+
+var options = {
+	type: "basic",
+	title: "My first popup with Chrome!",
+	message: "This is my messagee",
+	iconUrl: "favicon.ico"
+}
+if(5>4) {
+	chrome.notifications.create(options, callback);
+}
+function callback() {
+	console.log("Popup Done!");
+}
+
+
+$("a").on("click", function() {
+
+var contact = $("#contact").val();
+
+// window.open("http://localhost","_blank");
+$.post( "http://localhost/reminder/reminder.php",function(data) {
+
+	console.log("we reached post function");
+	alert(data);
+
+
+});
+
+
+});
